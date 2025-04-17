6685df9dc186e7d2ee22531f211f20e317e4e83c
Aulo Cezar <Aulofferreira@gmail.com>
2025-04-17 09:45:01 -0300

    Primeiro commit

---
+++ Main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+// Código para revisar tudo sobre O Embarcatech, e aprender mais, seguir as novas sugestões
+// do professor Wilton e Ricardo
+
+int main()
+{
+    stdio_init_all();
+
+    while (true)
+    {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
